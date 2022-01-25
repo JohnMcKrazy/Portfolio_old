f26689b93c740752aa6319992066d7c67e5aca8b
John McKrazy <gc.john.dcg@gmail.com>
2022-01-24 22:22:53 -0600

    clients change

--- assets/js/index.js
+++ assets/js/index.js
@@ -41,9 +41,9 @@ document.addEventListener('DOMContentLoaded', () => {
     //! ***********************************************************************************************//
     //!GENERAL VARIANTS--START
     const sunIcon =
-        '<svg class="theme_icon_svg" id="sun_icon_svg"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path class="cls-1" d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"/></svg>';
+        '<svg class="theme_icon_svg" id="sun_icon_svg"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Tema día</title><path class="cls-1" d="M12 18a6 6 0 1 1 0-12 6 6 0 0 1 0 12zM11 1h2v3h-2V1zm0 19h2v3h-2v-3zM3.515 4.929l1.414-1.414L7.05 5.636 5.636 7.05 3.515 4.93zM16.95 18.364l1.414-1.414 2.121 2.121-1.414 1.414-2.121-2.121zm2.121-14.85l1.414 1.415-2.121 2.121-1.414-1.414 2.121-2.121zM5.636 16.95l1.414 1.414-2.121 2.121-1.414-1.414 2.121-2.121zM23 11v2h-3v-2h3zM4 11v2H1v-2h3z"/></svg>';
     const moonIcon =
-        '<svg class="theme_icon_svg" id="moon_icon_svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path class="cls-1" d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z"/></svg>';
+        '<svg class="theme_icon_svg" id="moon_icon_svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Tema noche</title><path class="cls-1" d="M11.38 2.019a7.5 7.5 0 1 0 10.6 10.6C21.662 17.854 17.316 22 12.001 22 6.477 22 2 17.523 2 12c0-5.315 4.146-9.661 9.38-9.981z"/></svg>';
     let menuStatus = 'close';
     let menuSocialStatus = 'close';
     //!GENERAL VARIANTS--OVER
@@ -120,7 +120,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //^SOCIAL MENU CLOSE EN BODY CLICK--START
     const closeByBodyClick = (e) => {
         if (menuSocialStatus === 'open' && e.target.id !== 'contact_menu_btn') {
-            console.log(e.target.id);
+            //*console.log(e.target.id);
             closeMenuSocial();
         }
     };
@@ -377,18 +377,16 @@ document.addEventListener('DOMContentLoaded', () => {
                     animateItem(currentItem, '1', 'translateX(0)');
                 });
             } else {
-                setTimeout(() => {
-                    animationLeftContainers.forEach((container) => {
-                        //*console.log(container.id);
-                        const currentItem = document.querySelector(`#${container.id}`);
-                        animateItem(currentItem, '0', 'translateX(-50%)');
-                    });
-                    animationRightContainers.forEach((container) => {
-                        //*console.log(container.id);
-                        const currentItem = document.querySelector(`#${container.id}`);
-                        animateItem(currentItem, '0', 'translateX(50%)');
-                    });
-                }, 2000);
+                animationLeftContainers.forEach((container) => {
+                    //*console.log(container.id);
+                    const currentItem = document.querySelector(`#${container.id}`);
+                    animateItem(currentItem, '0', 'translateX(-50%)');
+                });
+                animationRightContainers.forEach((container) => {
+                    //*console.log(container.id);
+                    const currentItem = document.querySelector(`#${container.id}`);
+                    animateItem(currentItem, '0', 'translateX(50%)');
+                });
             }
             /* if (entry.isIntersecting) {
                 animationLeftContainers.forEach((container) => {
